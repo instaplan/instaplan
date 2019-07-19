564ecb38e68a3776f09e72dd7f723704c2c40e27
Marcio Silva <marciotexas@yahoo.com>
2019-07-19 15:57:36 -0500

    modify code

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,6 @@ import MyEvents from './Components/Myevents'
 export default (
    <Switch>
       <Route component={Landing} exact path='/' />
-      <Route component={Authorization} path='/auth' />
       <Route component={Authorization} path='/events/create' />
       <Route component={ViewEvent} path='/events/:eventId' />
       <Route component={BrowseEvents} path='/events' />
